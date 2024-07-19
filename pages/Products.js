c496f48005b97ec9f5309166d9289a787e897e19
brrna <bern.varol@gmail.com>
2024-07-19 14:12:45 +0300

    react-native-config added

--- pages/Products.js
+++ pages/Products.js
@@ -1,10 +1,15 @@
 import { SafeAreaView, StyleSheet, Text } from 'react-native'
 import React from 'react'
+import { API_URL } from '@env';
 
 const Products = () => {
+
+  const apiUrl = API_URL
+
   return (
     <SafeAreaView style={styles.container} >
       <Text>Products</Text>
+      <Text>{apiUrl}</Text>
     </SafeAreaView>
   )
 }
