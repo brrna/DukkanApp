a7aa2d8dbf87a56adb612e5f96d27ad9208422b9
brrna <bern.varol@gmail.com>
2024-07-19 14:48:28 +0300

    responsive screen added

---
+++ components/ProductCard.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen'
+
+const ProductCard = () => {
+  return (
+    <View>
+      <Text>ProductCard</Text>
+    </View>
+  )
+}
+
+export default ProductCard
+
+const styles = StyleSheet.create({})
