3da247db4a32b4fbd8d4963bad7c24f41f80634e
brrna <bern.varol@gmail.com>
2024-07-24 13:27:41 +0300

    splash scrren created

---
+++ pages/Splash.js
@@ -0,0 +1,44 @@
+import { Image, SafeAreaView, StyleSheet, Text } from 'react-native'
+import React, { useEffect } from 'react'
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen'
+import { useNavigation } from '@react-navigation/native'
+
+const Splash = () => {
+
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        setTimeout(() => {
+            navigation.navigate("LoginScreen")
+        }, 2000)
+    }, [])
+
+  return (
+    <SafeAreaView style={styles.container} >
+      <Image 
+        style={styles.image}
+        source={require("../assets/images/logo.jpeg")} />
+        <Text style={styles.text} > DUKKAN </Text>
+    </SafeAreaView>
+  )
+}
+
+export default Splash
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "rgb(241, 228, 212)",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    image: {
+        height: hp(30),
+        width: wp(60)
+    },
+    text: {
+        fontSize: hp(6),
+        fontWeight: "bold",
+        color: "rgb(47, 18, 0)"
+    }
+})
--- pages/router/Router.js
+++ pages/router/Router.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Products from '../Products';
 import Detail from '../Detail';
+import Splash from '../Splash';
+import Login from '../Login';
 
 const Router = () => {
 
@@ -9,6 +11,8 @@ const Router = () => {
 
   return (
     <Stack.Navigator screenOptions={{headerShown: false}} >
+        <Stack.Screen name='SplashScreen' component={Splash} />
+        <Stack.Screen name='LoginScreen' component={Login} />
         <Stack.Screen name='ProductsScreen' component={Products} />
         <Stack.Screen name='DetailScreen' component={Detail} />
     </Stack.Navigator>
