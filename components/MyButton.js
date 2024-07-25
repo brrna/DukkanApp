e2870edaca4021e073a1e24c91ba9a584b99b203
brrna <bern.varol@gmail.com>
2024-07-25 11:07:47 +0300

    component designed, formik added

--- components/MyButton.js
+++ components/MyButton.js
@@ -3,16 +3,17 @@ import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen'
 
-const MyButton = ({onPress}) => {
+const MyButton = ({ onPress }) => {
 
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
   return (
     <View style={styles.frame} >
       <Pressable
+        style={styles.button}
         onPress={onPress} >
-            <Text style={styles.text} >Sign In</Text>
-        </Pressable>
+        <Text style={styles.text} >Sign In</Text>
+      </Pressable>
     </View>
   )
 }
@@ -20,16 +21,26 @@ const MyButton = ({onPress}) => {
 export default MyButton
 
 const styles = StyleSheet.create({
-    frame: {
-        width: wp(100),
-        height: hp(10),
-        backgroundColor: "rgb(241, 228, 212)",
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    text: {
-        color: "rgb(201, 157, 120)",
-        fontWeight: "bold",
-        fontSize: hp(3)
-    }
+  frame: {
+    width: wp(100),
+    height: hp(10),
+    backgroundColor: "rgb(241, 228, 212)",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  button: {
+    backgroundColor: "rgb(201, 157, 120)",
+    height: hp(6,5),
+    width: wp(93),
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: wp(2),
+    borderColor: "rgb(252, 246, 234)",
+    borderWidth: 1
+  },
+  text: {
+    color: "rgb(252, 246, 234)",
+    fontWeight: "bold",
+    fontSize: hp(3)
+  }
 })
--- pages/Login.js
+++ pages/Login.js
@@ -1,67 +1,41 @@
-import { SafeAreaView, StyleSheet, Image, Alert } from 'react-native'
-import React, { useState } from 'react'
+import { SafeAreaView, StyleSheet, Image, View } from 'react-native'
+import React from 'react'
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen'
 import MyInput from '../components/MyInput'
 import MyButton from '../components/MyButton'
 import { useNavigation } from '@react-navigation/native'
-import axios from 'axios'
-import {LOGIN_URL} from "@env"
+import { Formik } from 'formik'
 
 const Login = () => {
 
     const navigation = useNavigation();
-
-    const [userName, setUserName] = useState("");
-    const [password, setPassword] = useState("");
-
-    const handleLogin = async () => {
-        try {
-            const response = await fetch(LOGIN_URL, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    username: userName,
-                    password: password,
-                }),
-            });
-
-            const json = await response.json();
-
-            if (response.ok) {
-                console.log('Login successful:', json);
-                // Navigate to the next screen (e.g., Home)
-                navigation.replace('ProductsScreen');
-            } else {
-                Alert.alert('Login Failed', json.message || 'An error occurred');
-            }
-        } catch (error) {
-            console.error('Error during login:', error);
-            Alert.alert('Login Error', 'Something went wrong. Please try again later.');
-        }
-    }
-
 
     return (
         <SafeAreaView style={styles.container} >
             <Image
                 style={styles.image}
                 source={require("../assets/images/logo.jpeg")} />
+            <Formik 
+                initialValues={{username: ' ', password: ' '}}
+                onSubmit={formValues => console.log(formValues) } >
+                {({handleSubmit, handleChange, values}) => (
+                    <View>
+                        <MyInput
+                            value={values.username}
+                            onChangeText={handleChange("username")}
+                            placeholder="enter your name"
+                            name="user" />
+                        <MyInput
+                            value={values.password}
+                            onChangeText={handleChange("password")}
+                            placeholder="enter your password"
+                            name="lock" />
 
-            <MyInput
-                value={userName}
-                onChangeText={setUserName}
-                placeholder="enter your name"
-                name="user" />
-            <MyInput
-                value={password}
-                onChangeText={setPassword}
-                placeholder="enter your password"
-                name="lock" />
-
-            <MyButton
-                onPress={handleLogin} />
+                        <MyButton
+                            onPress={handleSubmit} />
+                    </View>
+                )}
+            </Formik>
         </SafeAreaView>
     )
 }
